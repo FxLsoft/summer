e486bff56e5a7789e9affb968a23e3f97b6a1ec6
冯小亮 <Xiaoliang.feng@lotuscars.com.cn>
2022-05-18 11:21:30 +0800

    添加依赖检查

---
+++ src/core/context/common/Async.ts
@@ -0,0 +1,50 @@
+/**
+ * Execute the callback the next time the browser is idle
+ */
+export let runWhenIdle: (callback: (idle: IdleDeadline) => void, timeout?: number) => IDisposable;
+
+export interface IDisposable {
+	dispose(): void;
+}
+
+export class IdleValue<T> {
+
+	private readonly _executor: () => void;
+	private readonly _handle: IDisposable;
+
+	private _didRun: boolean = false;
+	private _value?: T;
+	private _error: unknown;
+
+	constructor(executor: () => T) {
+		this._executor = () => {
+			try {
+				this._value = executor();
+			} catch (err) {
+				this._error = err;
+			} finally {
+				this._didRun = true;
+			}
+		};
+		this._handle = runWhenIdle(() => this._executor());
+	}
+
+	dispose(): void {
+		this._handle.dispose();
+	}
+
+	get value(): T {
+		if (!this._didRun) {
+			this._handle.dispose();
+			this._executor();
+		}
+		if (this._error) {
+			throw this._error;
+		}
+		return this._value!;
+	}
+
+	get isInitialized(): boolean {
+		return this._didRun;
+	}
+}
